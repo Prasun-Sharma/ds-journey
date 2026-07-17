c7243c251cd5b4ab12b411d308f29f41c10b53b0
Prasun-Sharma <prasunsharma23@email.com>
2026-07-17 21:21:42 +0530

    Day 1 python practice

---
+++ Day01/TestWork.py
@@ -0,0 +1,55 @@
+print ("hello prasun sharma")
+
+name = "prasun" 
+age = 29
+height = '166' " " 'cm'
+weight = '68' " " 'kg'
+city = "gurugram"
+salary = 756000
+is_learning = True
+
+print(name)
+print(age)
+print(height)
+print(weight)
+print(city)
+print(salary)
+print(is_learning)  
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(weight))
+print(type(city))
+print(type(salary))
+print(type(is_learning))
+
+
+# # print
+# joke = pyjokes.get_joke()
+# print(joke)
+# import pyjokes
+# joke = pyjokes.get_joke()
+# print = (joke)
+
+# writing poem
+print ('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?''')
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("wait a minute who are you")
+engine.runAndWait()
+
---
+++ Day01/jokes.py
@@ -0,0 +1,14 @@
+import pyjokes
+print(pyjokes.get_joke())
+
+print("hello data science")
+
+name = "prasun"
+age = 29
+height = 166 
+is_working = True
+
+print(name)
+print(age)
+print(height)
+print(is_working)
